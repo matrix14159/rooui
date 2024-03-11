838289761b218a63ed3938e3eda2b0a3913bfcd3
Rison <944749471@qq.com>
2024-03-11 20:53:32 +0800

    impl createElm(continue)

--- vdom/patch.go
+++ vdom/patch.go
@@ -2,6 +2,8 @@ package vdom
 
 import (
 	"log/slog"
+	"math"
+	"strings"
 
 	"honnef.co/go/js/dom/v2"
 )
@@ -32,14 +34,13 @@ func (p *Patcher) Patch(newNode *VNode) (err error) {
 	if SameVNode(p.curVNode, newNode) {
 		p.patchVNode(newNode)
 	} else {
-
 		elm := p.curVNode.Elm
 		parent := p.api.ParentNode(elm)
 
 		newNode.Elm = p.createElm(newNode)
 
 		slog.Info("not the same vnode, replace current element",
-			"curVNode", *p.curVNode, "parent", parent.NodeName())
+			"curVNode", *p.curVNode, "parent", parent.NodeName(), "newNode", newNode)
 
 		if parent != nil {
 			p.api.InsertBefore(parent, newNode.Elm, p.api.NextSibling(newNode.Elm))
@@ -59,10 +60,44 @@ func (p *Patcher) createElm(vnode *VNode) dom.Node {
 		return p.api.CreateTextNode(vnode.Text)
 	}
 
-	//hashIdx := strings.Index(vnode.Sel, "#")
-	//dotIdx := strings.Index(vnode.Sel, ".")
+	hashIdx := strings.Index(vnode.Sel, "#")
+	hash := len(vnode.Sel)
+	if hashIdx > 0 {
+		hash = hashIdx
+	}
 
-	return nil
+	dotIdx := strings.Index(vnode.Sel, ".")
+	dot := len(vnode.Sel)
+	if dotIdx > 0 {
+		dot = dotIdx
+	}
+
+	tag := vnode.Sel
+	if hashIdx != -1 || dotIdx != -1 {
+		minLen := math.Min(float64(hash), float64(dot))
+		tag = vnode.Sel[0:int(minLen)]
+	}
+	slog.Info("createElm", "tag", tag)
+
+	var elm dom.HTMLElement
+	if vnode.Data != nil && vnode.Data.Namespace != "" {
+		elm = p.api.CreateElementNS(vnode.Data.Namespace, tag)
+	} else {
+		elm = p.api.CreateElement(tag)
+	}
+	vnode.Elm = elm
+
+	if hash < dot {
+		slog.Info("createElm", "id", vnode.Sel[hash+1:dot])
+		elm.SetAttribute("id", vnode.Sel[hash+1:dot])
+	}
+
+	if dotIdx > 0 {
+		cls := strings.Replace(vnode.Sel[dot+1:], ".", " ", -1)
+		slog.Info("createElm", "raw-class", vnode.Sel[dot+1:], "use-class", cls)
+		elm.SetAttribute("class", cls)
+	}
+	return vnode.Elm
 }
 
 func (p *Patcher) removeVNodes(parentElm dom.Node, vnodes []*VNode, startIdx, endIdx int) {
--- vdom/patch_test.go
+++ vdom/patch_test.go
@@ -8,7 +8,33 @@ import (
 func TestPatch(t *testing.T) {
 	root := insertDiv(t, "root")
 	p := NewPatcher(NewStandardDomApi(), EmptyNodeAt(root))
-	err := p.Patch(&VNode{})
+
+	vnode := H("div", nil, "hello", nil)
+	err := p.Patch(vnode)
+	if err != nil {
+		return
+	}
+}
+
+// GOOS=js GOARCH=wasm go test -run TestPatch2
+func TestPatch2(t *testing.T) {
+	root := insertDiv(t, "root")
+	p := NewPatcher(NewStandardDomApi(), EmptyNodeAt(root))
+
+	vnode := H("div#app", nil, "hello", nil)
+	err := p.Patch(vnode)
+	if err != nil {
+		return
+	}
+}
+
+// GOOS=js GOARCH=wasm go test -run TestPatch3
+func TestPatch3(t *testing.T) {
+	root := insertDiv(t, "root")
+	p := NewPatcher(NewStandardDomApi(), EmptyNodeAt(root))
+
+	vnode := H("div.btn.mart24", nil, "hello", nil)
+	err := p.Patch(vnode)
 	if err != nil {
 		return
 	}
