84928667e563264e033eef7ae426bec8bd2f6d1f
Rison <944749471@qq.com>
2025-06-06 18:13:21 +0800

    remove un-use

--- dom/error.go
+++ dom/error.go
@@ -1,7 +1,6 @@
 package dom
 
 import (
-	"fmt"
 	"reflect"
 	"syscall/js"
 )
@@ -10,16 +9,6 @@ import (
 func NewError(goErr error) js.Value {
 	errConstructor := js.Global().Get("Error")
 	return errConstructor.New(goErr.Error())
-}
-
-// TypeMismatchError is returned when a function is called with a js.Value that has the incorrect type.
-type TypeMismatchError struct {
-	Expected js.Type
-	Actual   js.Type
-}
-
-func (e TypeMismatchError) Error() string {
-	return fmt.Sprintf("expected %v type, got %v type instead", e.Expected, e.Actual)
 }
 
 var errorType = reflect.TypeOf((*error)(nil)).Elem()
